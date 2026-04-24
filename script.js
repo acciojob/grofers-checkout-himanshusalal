1b215c67aaed9c33345178e33cca09ae7bc0a917
himanshusalal <himanshusalal517@gmail.com>
2026-04-24 11:04:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-<Script>
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.innerText = "Get Total Price";
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+
+    let prices = document.querySelectorAll(".prices");
+
+    let total = 0;
+
+    prices.forEach(function(price) {
+        total += Number(price.innerText);
+    });
+
+    let table = document.querySelector("#groceryTable");
+
+    let newRow = document.createElement("tr");
+
+    let newCell = document.createElement("td");
+
+    newCell.colSpan = 2;
+    newCell.innerText = "Total Price: " + total;
+
+    newRow.appendChild(newCell);
+
+    table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
-
-// Select all prices
-let prices = document.querySelectorAll(".prices");
-
-let total = 0;
-
-// Calculate total dynamically
-prices.forEach(function(price) {
-    total += Number(price.innerText);
-});
-
-// Select table
-let table = document.querySelector("#groceryTable");
-
-// Create new row
-let newRow = document.createElement("tr");
-
-// Create new cell
-let newCell = document.createElement("td");
-
-// Span across 2 columns
-newCell.colSpan = 2;
-
-// Add total text
-newCell.innerText = "Total Price: " + total;
-
-// Append cell to row
-newRow.appendChild(newCell);
-
-// Append row to table
-table.appendChild(newRow);
-
-</script>
